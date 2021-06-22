b6d8b009b1318c809e9cb6ce51c93d5a491e1828
Joseph Blais <josephpierreblais@gmail.com>
2021-06-22 14:49:46 -0400

    Move button styling to seperate file

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
+import "./Button.css"
+
 export default function Button(props) {
   const { text, click } = props
 
   return (
     <button 
-      style={{backgroundColor: "white", color: "#001f97", fontSize: "2em"}}
       onClick={click}
     >{text}</button>
   )
